7862d7eba110db67a920d07d4645d0594c76ed79
Victor Storozhenko <storozvic@gmail.com>
2019-11-20 15:31:18 -0800

    keys added

--- src/config_sample.js
+++ src/config_sample.js
@@ -1,12 +1,12 @@
 export default {
 	apiGateway: {
-		REGION: 'YOUR_API_GATEWAY_REGION',
-		URL: 'YOUR_API_GATEWAY_URL'
+		REGION: 'us-east-1',
+		URL: 'https://1rualds5vk.execute-api.us-east-1.amazonaws.com/dev'
 	},
 	cognito: {
-		REGION: 'YOUR_COGNITO_REGION',
-		USER_POOL_ID: 'YOUR_COGNITO_USER_POOL_ID',
-		APP_CLIENT_ID: 'YOUR_COGNITO_APP_CLIENT_ID',
-		IDENTITY_POOL_ID: 'YOUR_IDENTITY_POOL_ID'
+		REGION: 'us-east-1',
+		USER_POOL_ID: 'us-east-1_jd5n2y8ky',
+		APP_CLIENT_ID: '5u7uc2mg6p8rpq2t07m52cd6ro',
+		IDENTITY_POOL_ID: 'us-east-1_jd5n2y8ky'
 	}
 };
